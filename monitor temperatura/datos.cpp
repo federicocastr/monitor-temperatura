8d465104e120e462a9f025e1498bffdc96e0ad2b
federicocastr <146401101+federicocastr@users.noreply.github.com>
2024-05-15 20:31:28 -0500

    modifique la funcion maxmin para que no mirara el historial cuando este este vacio

--- monitor temperatura/datos.cpp
+++ monitor temperatura/datos.cpp
@@ -2,45 +2,45 @@
 ifstream buscart;
 ifstream buscarh;
 ofstream historial;
-datos::datos ( ) {
-	this->temperatura=0;
-	this->estado="";
-	this->tempMax=0;
-	this->tempMin=0;
+datos::datos() {
+	this->temperatura = 0;
+	this->estado = "";
+	this->tempMax = 0;
+	this->tempMin = 0;
 }
 
-datos::datos (string nombre, string apellido, int cedula, int edad, string genero, float temperatura, string estado, float tempMax, float tempMin)
-:paciente(nombre,apellido,cedula,edad,genero){
-	this->temperatura=temperatura;
-	this->estado=estado;
-	this->tempMax=tempMax;
-	this->tempMin=tempMin;
+datos::datos(string nombre, string apellido, int cedula, int edad, string genero, float temperatura, string estado, float tempMax, float tempMin)
+	:paciente(nombre, apellido, cedula, edad, genero) {
+	this->temperatura = temperatura;
+	this->estado = estado;
+	this->tempMax = tempMax;
+	this->tempMin = tempMin;
 }
-datos::datos(string nombre,string apellido, int cedula, int edad, string genero)
-	:paciente(nombre,apellido,cedula,edad,genero){
+datos::datos(string nombre, string apellido, int cedula, int edad, string genero)
+	:paciente(nombre, apellido, cedula, edad, genero) {
 }
 
 datos::~datos()
 {
 }
 
-void datos::settemperatura (float temperatura) {
-	this->temperatura=temperatura;
+void datos::settemperatura(float temperatura) {
+	this->temperatura = temperatura;
 }
 
-void datos::setestado (string estado) {
-	this->estado=estado;
+void datos::setestado(string estado) {
+	this->estado = estado;
 }
 
-void datos::settempMax (float tempMax) {
-	this->tempMax=tempMax;
+void datos::settempMax(float tempMax) {
+	this->tempMax = tempMax;
 }
 
-void datos::settempMin (float tempMin) {
-	this->tempMin=tempMin;
+void datos::settempMin(float tempMin) {
+	this->tempMin = tempMin;
 }
 
-float datos::gettemperatura ( ) {
+float datos::gettemperatura() {
 	return temperatura;
 }
 
@@ -59,80 +59,82 @@ float datos::gettempMin()
 	return tempMin;
 }
 
-void datos::prom () {	
+void datos::prom() {
 	ifstream archivo("temperaturas.txt");
-	
-	float x,numt=0,prom=0,sum=0;
-	while (!archivo.eof()){
+
+	float x, numt = 0, prom = 0, sum = 0;
+	while (!archivo.eof()) {
 		numt++;
-		archivo>>x;
-		sum+=x;
-		prom=sum/numt;
+		archivo >> x;
+		sum += x;
+		prom = sum / numt;
 	}
-	settemperatura(prom);	
+	settemperatura(prom);
 }
 
-void datos::Estado () { 
-	
+void datos::Estado() {
+
 	string estad;
-	if(temperatura<35){
-		estad="HIPOTERMIA";
+	if (temperatura < 35) {
+		estad = "HIPOTERMIA";
 	}
-	else{
-		if(35<=temperatura && temperatura<=37){
-			estad="ESTABLE";
+	else {
+		if (35 <= temperatura && temperatura <= 37) {
+			estad = "ESTABLE";
 		}
-		else{
-			if(temperatura>37){
-				estad="FIEBRE";
+		else {
+			if (temperatura > 37) {
+				estad = "FIEBRE";
 			}
 		}
 	}
 	setestado(estad);
-	
+
 }
-void datos::maxmin () {
-	float max=0;
-	float min=999;
-	int cedu;
+void datos::maxmin() {
+	float max = 0;
+	float min = 999;
+	int cedu, h;
 	float m;
 	float tmax, tmin, temp;
 	string est;
 	string d;
-	buscart.open("temperaturas.txt",ios::in);
-	while(!buscart.eof()){
+	buscart.open("temperaturas.txt", ios::in);
+	while (!buscart.eof()) {
 		buscart >> m;
-		if(m>=max){
-			max=m;
+		if (m >= max) {
+			max = m;
 		}
-		if(m<=min){
-			min=m;
+		if (m <= min) {
+			min = m;
 		}
 	}
 	buscart.close();
 	buscarh.open("historial.txt", ios::in);
-		buscarh >> cedu;
+	buscarh >> cedu;
+	if (cedu != 0) {
 		buscarh >> tmax;
-		if (tmax > max) {
-			max = tmax;
-		}
-		if (!buscarh.eof()) {
-			buscarh >> tmin;
-			buscarh >> temp;
-			buscarh >> est;
-			buscarh >> m;
+		buscarh >> tmin;
+		buscarh >> temp;
+		buscarh >> est;
+		buscarh >> m;
+		if (cedu != 0) {
+			if (tmax > max) {
+				max = tmax;
+			}
 			if (tmin < min) {
 				min = tmin;
 			}
 		}
+	}
 	settempMax(max);
 	settempMin(min);
 }
-void datos::guardar(){
-	historial.open("historial.txt",ios::in);
-	historial <<cedula<<endl;
-	historial<<tempMax<<endl;
-	historial<<tempMin<<endl;
-	historial<<temperatura<<endl;
-	historial<<estado<<endl;
+void datos::guardar() {
+	historial.open("historial.txt", ios::in);
+	historial << cedula << endl;
+	historial << tempMax << endl;
+	historial << tempMin << endl;
+	historial << temperatura << endl;
+	historial << estado << endl;
 }
